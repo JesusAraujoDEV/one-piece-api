5a81d17804572a6e71201b1a8cc60b33dfc7aa6a
JesusAraujoDEV <j2a0a0a5@gmail.com>
2025-02-26 00:13:26 -0400

    Comment out pirates and users routers for future reference

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const frutasRouter = require('./frutas_router');
-const piratesRouter = require('./pirates_router');
-const usersRouter = require('./users_router');
+//const piratesRouter = require('./pirates_router');
+//const usersRouter = require('./users_router');
 
 
 
@@ -10,8 +10,8 @@ function routerApi(app){
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/frutas', frutasRouter);
-  router.use('/pirates', piratesRouter);
-  router.use('/users', usersRouter);
+  //router.use('/pirates', piratesRouter);
+  //router.use('/users', usersRouter);
 
 }
 
