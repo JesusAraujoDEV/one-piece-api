e9c556a64109d4058b0837a09680dc89d546ce72
JesusAraujoDEV <j2a0a0a5@gmail.com>
2025-02-25 23:47:05 -0400

    Comment out routes in pirates_router and users_router for future reference

--- api/routes/users_router.js
+++ api/routes/users_router.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const { faker } = require('@faker-js/faker');
+// const express = require('express');
+// const { faker } = require('@faker-js/faker');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if(limit && offset) {
-    res.json(
-      {
-        limit,
-        offset
-      }
-    )
-  } else{
-    res.json('No hay limit ni offset');
-  }
-});
+// router.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if(limit && offset) {
+//     res.json(
+//       {
+//         limit,
+//         offset
+//       }
+//     )
+//   } else{
+//     res.json('No hay limit ni offset');
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
