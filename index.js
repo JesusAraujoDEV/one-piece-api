9437473740bc7ecb375df6740772063b6277a4c4
JesusAraujoDEV <j2a0a0a5@gmail.com>
2025-02-25 22:44:39 -0400

    Add CORS support and create prueba.html for API testing

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const routerApi = require('./routes');
 const swaggerDocs = require('./swagger');
+const cors = require('cors');
 
 const { logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error_handler');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)){
+      callback(null, true);
+    } else{
+      callback(new Error('No permitido por CORS'));
+    }
+  }
+}
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('HOLA MUNDOOOO, primer get en express')
