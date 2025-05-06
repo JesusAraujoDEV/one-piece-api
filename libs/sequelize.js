d0d2a1e2a85dc4ef203175e7eb46f0f0d93a0722
jesusaraujodev <j2a0a0a5@gmail.com>
2025-05-06 01:16:44 -0400

    Update database configuration to use PostgreSQL, enhance user model with role attribute, and add migration scripts to package.json

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,16 +2,15 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const { setupModels } = require('./../db/models');
 
-const USER = encodeURIComponent(config.mysql.user);
-const PASSWORD = encodeURIComponent(config.mysql.password);
-const URI = `mysql://${USER}:${PASSWORD}@${config.mysql.host}:${config.mysql.port}/${config.mysql.database}`;
+const USER = encodeURIComponent(config.postgres.user);
+const PASSWORD = encodeURIComponent(config.postgres.password);
+const URI = `postgres://${USER}:${PASSWORD}@${config.postgres.host}:${config.postgres.port}/${config.postgres.database}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: true,
 });
 
 setupModels(sequelize);
-sequelize.sync();
 
 module.exports = sequelize;
