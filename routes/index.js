5c570b8995c4ba10be650ee04d1c8ed6c37fc087
jesusaraujodev <j2a0a0a5@gmail.com>
2025-05-12 16:30:11 -0400

    Add Category and Product models with routes integration, enhancing the database structure and API endpoints.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const frutasRouter = require('./frutas_router');
 const piratesRouter = require('./pirates_router');
 const usersRouter = require('./users_router');
 const customersRouter = require('./customer_router');
-
-
+const productsRouter = require('./product_router');
+const categoryRouter = require('./category_router');
 
 function routerApi(app){
   const router = express.Router();
@@ -14,6 +14,8 @@ function routerApi(app){
   router.use('/pirates', piratesRouter);
   router.use('/users', usersRouter);
   router.use('/customers', customersRouter);
+  router.use('/categories', categoryRouter);
+  router.use('/products', productsRouter);
 
 }
 
