76a3f5f9d1679233f6fa247a1c675c6df03fa087
jesusaraujodev <j2a0a0a5@gmail.com>
2025-06-07 23:42:37 -0400

    Feat: Implement password hashing for user and customer creation in services. Utilize bcrypt to securely hash passwords before storing them in the database, enhancing security for user credentials.

--- services/customer_service.js
+++ services/customer_service.js
@@ -1,14 +1,25 @@
 const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
+const bcrypt = require('bcrypt');
+const { password } = require('pg/lib/defaults');
 
 class CustomerService {
 
   constructor(){
   }
   async create(data) {
-    const newCustomer = await models.Customer.create(data, {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    const newData = {
+      ...data,
+      user:{
+        ...data.user,
+        password: hash
+      }
+    }
+    const newCustomer = await models.Customer.create(newData, {
         include: ['user']
     });
+    delete newCustomer.user.dataValues.password;
     return newCustomer;
   }
 
